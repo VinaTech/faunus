55ad069f8cc6ec891c3900c1f7e9605ec9d9a50a
nguyentrucdn <nguyentrucdn@gmail.com>
2013-08-22 14:34:59 +0700

    Truc - fix build

--- src/test/java/com/thinkaurelius/faunus/formats/sequence/faunus01/FaunusSequenceFileRecordReaderTest.java
+++ src/test/java/com/thinkaurelius/faunus/formats/sequence/faunus01/FaunusSequenceFileRecordReaderTest.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapred.TaskAttemptID;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,7 +21,7 @@ public class FaunusSequenceFileRecordReaderTest extends BaseTest {
     public void testRecordReader() throws Exception {
         FaunusSequenceFileRecordReader reader = new FaunusSequenceFileRecordReader();
         reader.initialize(new FileSplit(new Path(FaunusSequenceFileRecordReaderTest.class.getResource("graph-of-the-gods-faunus01.seq").toURI()), 0, Long.MAX_VALUE, new String[]{}),
-                new TaskAttemptContext(new Configuration(), new TaskAttemptID()));
+                new TaskAttemptContextImpl(new Configuration(), new TaskAttemptID()));
 
         int counter = 0;
         final Map<Long, FaunusVertex> graph = new HashMap<Long, FaunusVertex>();
